db155713db307a1f6e8f7c79f390b90eab092ec1
Kovács Milán <95681544+Milkacola@users.noreply.github.com>
2022-06-05 21:21:20 +0200

    packagek létrehozása, metódus kiszervezés

---
+++ src/main/java/teszt/KincsesTeszt.java
@@ -0,0 +1,14 @@
+package teszt;
+
+public class KincsesTeszt {
+    
+    public static void main(String[] args) {
+
+        System.out.println("Teszt");
+        
+        
+    
+    
+    }
+    
+}
